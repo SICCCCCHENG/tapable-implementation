d5b6156454fc8f312d8fa861308376153d6709da
sicheng <zsc0373@outlook.com>
2025-08-16 16:23:51 +0800

    tapable init

---
+++ index.js
@@ -0,0 +1,22 @@
+const { SyncHook } = require("./tapable");
+let syncHook = new SyncHook(["name", "age"]);
+
+syncHook.tap({ name: '1' }, (name, age) => {
+    console.log(1, name, age);
+});
+
+syncHook.tap("2", (name, age) => {
+    console.log(2, name, age);
+});
+
+syncHook.call("zhufeng", 10);
+
+/**
+(function anonymous(name, age) {
+    var _x = this._x;
+    var _fn0 = _x[0];
+    _fn0(name, age);
+    var _fn1 = _x[1];
+    _fn1(name, age);
+})
+*/
